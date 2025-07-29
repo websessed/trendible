a4753134cc95e7f20d75d9acf713b60c614fc1ee
Maxwell Easter <maxwelleaster00@gmail.com>
2025-07-29 01:09:29 +0100

    update folder

---
+++ desc.js
@@ -0,0 +1,39 @@
+/*
+
+version 1: 
+    -user auth 
+    -personal feed (based on interest)
+    -trend item + metadata (title, desc, mentions, source)
+    -trend data (key people, trend stats, tools, threads)
+    -sort by (popularity, recency)
+    -vote (upvote/downvote)
+    -trend score algorithm 
+    -bookmark
+    -settings & notifs
+
+
+version 2: 
+    -trendible AI (summary, analysis)
+
+
+version 3: 
+    -comments 
+
+
+version 4: 
+    -finance news feed 
+
+
+user feed: 
+    -stocks, crypto, airdrops
+    -products, services 
+    -startups 
+
+
+signal sources: 
+    -twitter 
+    -google finance
+    -coinbase
+    -tiktok / youtube 
+
+*/
